5b9f100b59aa6f61d2cae7067cc6c6f39ab7b7e6
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-04-20 12:28:20 +0530

    Add some notes for future me

--- src/index.js
+++ src/index.js
@@ -9,5 +9,20 @@ const todos = data.getToDos();
 renderToDos(todos);
 
 /*
-IMO, the code could be better structured. The data structure used in data.js isn't natural and render.js needs access to what the current display context is e.g. if you click new todo on the groceries tab it should know that so it can add it to the correct folder.
+IMO, the code could be better structured. 
+The data structure used in data.js isn't natural and render.js 
+needs access to what the current display context is e.g. if you 
+click new todo on the groceries tab it should know that so it can 
+add it to the correct folder.
+
+The date display is also weird because date-fns library's formatDistance is strange.
+There might be a way to strip hour data form the dates.
+
+missing features: 
+
+* delete todo
+* drag and drop
+* testing
+* persistent storage
+* probably just redo the whole thing in react 
 */
